b82763c13691fcc2a1bf9591ac4baa3c63358ba1
Nyan Htet <nyanhtet@yahoo.com>
2022-01-20 00:05:55 -0800

    added categories selection to frontpage

--- templates/frontpage.php
+++ templates/frontpage.php
@@ -1,5 +1,17 @@
 <?php include 'inc/header.php'?>
 
+    <div class="jumbotron">
+        <h1>Find a listing: </h1>
+        <form>
+            <select name="category" class="form-control">
+                <option value="0"> Choose Category </option>
+                <?php foreach($categories as $category): ?>
+                    <option value="<?php echo $category->id; ?>"><?php echo $category->name; ?></option>
+                <?php endforeach; ?>
+            </select>
+        </form>
+    </div>
+
     <?php foreach($listings as $list): ?>
       <div class="row marketing">
         <div class="col-md-10">
