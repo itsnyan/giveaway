8ee232763d2fce1daba82207e4acbcf9c81821c9
Nyan Htet <nyanhtet@yahoo.com>
2022-01-20 00:05:40 -0800

    added listings query to frontpage

--- templates/frontpage.php
+++ templates/frontpage.php
@@ -1,5 +1,16 @@
 <?php include 'inc/header.php'?>
 
+    <?php foreach($listings as $list): ?>
+      <div class="row marketing">
+        <div class="col-md-10">
+          <h4><?php echo $list->title." (".$list->location.")"; ?></h4>
+          <p><?php echo $list->description; ?></p>
+        </div>
+        <div class="col-md-2">
+            <a class="btn btn-primary" href="#">View</a>
+        </div>
+    </div>
+    <?php endforeach; ?>
  
 
 
