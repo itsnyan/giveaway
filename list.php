902a49b9d90fdb2e2affcde0f7f86f0cfdb2d7be
Nyan Htet <nyanhtet@yahoo.com>
2022-01-20 09:01:51 -0800

    Update list.php

--- list.php
+++ list.php
@@ -2,6 +2,10 @@
 
 <?php 
 
+$listing = new Listing;
+$template = new Template('templates/single-list.php');
+
+//if there's a category param in the URL set to the var, if not set to null
 $list_id = isset($_GET['id']) ? $_GET['id'] : null;
 
 $template->listing = $listing->getListing($list_id);
