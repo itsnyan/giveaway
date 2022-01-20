531f3204dbff06557eac89073af1a69b282bf2d6
Nyan Htet <nyanhtet@yahoo.com>
2022-01-20 00:04:32 -0800

    added header

---
+++ templates/inc/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">    <link rel="stylesheet" href="css/styles.css">
+
+    <title>GiveAway</title>
+</head>
+
+<div class="container">
+      <div class="header clearfix">
+        <nav>
+          <ul class="nav nav-pills pull-right">
+            <li role="presentation" ><a href="index.php">Home</a></li>
+            <li role="presentation"><a href="create.php">Create Listing</a></li>
+            <li role="presentation"><a href="#">Contact</a></li>
+          </ul>
+        </nav>
+        <h3 class="text-muted"><?php echo SITE_TITLE; ?></h3>
+      </div>
